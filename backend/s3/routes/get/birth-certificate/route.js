36bf8fa3d1cc07d811b77bf29eb94cc2dd27504a
Madeline Savoiu <savoiu@csu.fullerton.edu>
2025-10-25 03:08:19 -0700

    Refactored get route

--- backend/s3/routes/get/birth-certificate/route.js
+++ backend/s3/routes/get/birth-certificate/route.js
@@ -1,11 +1,40 @@
-const express = require('express');
-const router = express.Router();
-const { getExampleData } = require('../controllers/exampleController');
-const verifyToken = require('../middleware/authMiddleware'); // optional
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import fs from "fs";
+import dotenv from "dotenv";
 
-// GET route
-router.get('/get/birth-certificate/:userId', verifyToken, (req, res) => {
+dotenv.config();
+
+const s3 = new S3Client({
+    region: process.env.AWS_REGION
+});
+
+router.get('/get/:document/:userId', verifyToken, async (req, res) => {
+    try {
+        const { document, userId }= req.params;
+
+        // Change later to read from React Native image upload library
+        const fileStream = fs.createReadStream(filePath);
+
+        const key = `user_uploads/${userId}/${document}_{timestamp}.pdf`;
+        
+        const command = new PutObjectCommand({
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: key,
+            Body: fileStream,
+            ContentType: "application/pdf",
+            ServerSideEncryption: "aws:kms", // SSE-KMS
+            SSEKMSKeyId: process.env.AWS_KMS_KEY_ARN,
+        });
+        
+        // Upload to S3 bucket
+        const result = await s3.send(command);
     
+        return res.json({ status: 201 }, { path: key, message: "Document uploaded successfully." });
+        
+    } catch (error) {
+        console.error(error.message);
+        return res.json({ status: 500 }, { message: "Server error" })
+    }
 });
 
 module.exports = router;
