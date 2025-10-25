6f14b911d0d7dde5ee1c80284008833cbf6812c2
Madeline Savoiu <savoiu@csu.fullerton.edu>
2025-10-25 02:27:48 -0700

    URL signing route

--- backend/s3/routes/auth/sign-url/route.js
+++ backend/s3/routes/auth/sign-url/route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import s3 from "../../../s3Client.js";
 
 const router = express.Router();
-const s3 = new S3Client({ region: process.env.AWS_REGION });
 
 // Secure: require your app auth middleware before this route!
 router.get("/presign", async (req, res) => {
